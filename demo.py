f90be8d569f579c5fcea2ac2ee5f7a8929a2666f
Weiyu Du <weiyudu@huntsman-ve703-01109.apn.wlan.private.upenn.edu>
2019-10-02 21:28:59 -0400

    compute_grad issue

--- demo.py
+++ demo.py
@@ -65,7 +65,7 @@ def main():
     NetClass = load_model('ResidualGRUNet')
 
     # Define a network and a solver. Solver provides a wrapper for the test function.
-    net = NetClass(compute_grad=False)  # instantiate a network
+    net = Net(compute_grad=False)  # instantiate a network
 
     net.load(DEFAULT_WEIGHTS)                        # load downloaded weights
     solver = Solver(net)                # instantiate a solver
